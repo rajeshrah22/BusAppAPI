b1a2bd180582a830d870408a5ed40848da137eac
rajeshrah22 <rajeshrah22@gmail.com>
2023-02-05 20:09:41 -0400

    testing  merge command

--- src/main/webapp/JS/index.js
+++ src/main/webapp/JS/index.js
@@ -51,14 +51,6 @@ function getRoutes(agencyTag) {
 console.log("JS is running");
 getAgencyLocations();
 
-/*// Create the script tag, set the appropriate attributes
-var script = document.createElement('script');
-script.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyDVbO9qu-JXbMHKL6jULNdrP1r3o8L0Q4g&callback=initMap&v=weekly';
-script.async = true;*/
-
-// Append the 'script' element to 'head'
-//document.head.appendChild(script);
-
 let map;
 
 function initMap() {
